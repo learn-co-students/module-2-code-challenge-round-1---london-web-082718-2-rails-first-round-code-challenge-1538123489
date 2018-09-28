1c3889f583f2c43058ef1b834ea7a0667697573a
github username <github email address>
2018-09-28 11:04:30 +0100

    done.

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,34 @@
 class HeroinesController < ApplicationController
+  before_action :set_powers, only: [:new, :edit]
   def index
     @heroines = Heroine.all
   end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def new
+    @heroine = Heroine.new
+  end
+
+  def create
+    @heroine = Heroine.create(heroine_params)
+    if @heroine.valid?
+      redirect_to @heroine
+    else
+      flash[:errors] = @heroine.errors.full_messages
+      redirect_to new_heroine_path
+    end
+  end
+
+  private
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power)
+  end
+
+  def set_powers
+    @powers = Power.all.sort_by { |r| r.name }
+  end
+
 end
--- app/models/heroine.rb
+++ app/models/heroine.rb
@@ -1,2 +1,6 @@
 class Heroine < ApplicationRecord
+  belongs_to :power
+
+  validates :super_name, uniqueness: true, presence: true
+  validates :power, uniqueness: true, presence: true
 end
