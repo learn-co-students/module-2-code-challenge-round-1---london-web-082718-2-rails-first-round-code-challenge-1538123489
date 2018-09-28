9961ad2c6c96ad7b34cf3f7af03d6092f8ccc03c
thexyman <makdaa1@gmail.com>
2018-09-28 11:04:06 +0100

    all tasks complete

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,41 @@
 class HeroinesController < ApplicationController
+
+  before_action :set_powers, only: [:new]
+
   def index
-    @heroines = Heroine.all
+    @heroines = if params[:power_name]
+        Heroine.all.select {|h| h.power.name == params[:power_name]}
+      else params[:super_name]
+        Heroine.all
+      end
   end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def new
+    @heroine = Heroine.new
+  end
+
+  def create
+    @heroine = Heroine.create(heroine_params)
+    if @heroine.valid?
+      redirect_to heroine_path(@heroine)
+    else
+      flash[:errors] = @heroine.errors.full_messages
+      redirect_to new_heroine_path
+    end
+  end
+
+  private
+
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id, :power_name)
+  end
+
+  def set_powers
+    @powers = Power.all.sort_by { |r| r.name }
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
-  resources :powers, only: [:index]
-  resources :heroines, only: [:index]  
+
+
+  resources :powers
+  resources :heroines
+
+  # #Powers
+  # resources :powers, only: [:index]
+  # get "/powers/:id/", to: "powers#show", as: "power"
+  #
+  # #heroines
+  # resources :heroines, only: [:index, :new]
+  # get "/heroines/:id/", to: "heroiness#show", as: "heroine"
+  #
+
+
+
 end
