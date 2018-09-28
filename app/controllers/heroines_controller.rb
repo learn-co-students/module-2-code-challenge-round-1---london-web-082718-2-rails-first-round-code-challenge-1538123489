ab8b6a7209a5dd5b90cacfd43e956c3aaa88f9fd
Ciaran M <cmorinan@gmail.com>
2018-09-28 11:03:02 +0100

    completed challenge

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,42 @@
 class HeroinesController < ApplicationController
+
+  before_action :get_powers, only: [:new, :create, :index]
+
   def index
-    @heroines = Heroine.all
+    if params[:q]
+      @heroines = Heroine.select { |heroine| heroine.power_name == params[:q] }
+    else
+      @heroines = Heroine.all
+    end
   end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def new
+    @heroine = Heroine.new
+    @powers = Power.all
+  end
+
+  def create
+    @heroine = Heroine.new(heroine_params)
+    if @heroine.valid?
+      @heroine.save
+      redirect_to @heroine
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
+  end
+
+  def get_powers
+    @powers = Power.all
+  end
+
 end
--- app/models/heroine.rb
+++ app/models/heroine.rb
@@ -1,2 +1,7 @@
 class Heroine < ApplicationRecord
+
+  belongs_to :power
+  delegate :name, to: :power, prefix: true
+  validates :super_name, uniqueness: true
+
 end
